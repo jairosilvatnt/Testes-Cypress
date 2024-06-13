961d2d4db39c5ab845859fcd6b135e2b4b0b291c
Jairo da Silva <silva_jairo2009@yahoo.com.br>
2024-06-13 15:05:23 -0300

    refatorando o codigo...

---
+++ cypress/e2e/novoTeste/elemento.js
@@ -0,0 +1,8 @@
+const elemento = 
+{
+  nome: "input[placeholder='Informe seu Nome']",
+  email: "form[class='styles__ContainerFormRegister-sc-7fhc7g-0 keVbpY'] input[placeholder='Informe seu e-mail']",
+  password: "form[class='styles__ContainerFormRegister-sc-7fhc7g-0 keVbpY'] input[placeholder='Informe sua senha']"
+}
+
+export default elemento
--- cypress/e2e/novoTeste/primeiroTeste.cy.js
+++ cypress/e2e/novoTeste/primeiroTeste.cy.js
@@ -1,40 +1,43 @@
+import elemento from "./elemento";
+
 /// <reference types="cypress" />
 
 
 describe('Meu primeiro projeto', () => {
+  const user_1 = "jairotest@gmail.com"
+  const name = "Jairo da silva"
+  const psw = 123456
+  const btnRegistrar = "button[class='style__ContainerButton-sc-1wsixal-0 ihdmxA button__child']"
+  const btnCadastrar = "button[class='style__ContainerButton-sc-1wsixal-0 CMabB button__child']"
+  const btnAcessar = "button[class='style__ContainerButton-sc-1wsixal-0 otUnI button__child']"
 beforeEach(() => {
   cy.visit('/')
   });
 
   it('Fazendo login', () => {
-    cy.get("button[class='style__ContainerButton-sc-1wsixal-0 ihdmxA button__child']").should('contain', 'Registrar').click();
+    cy.get(btnRegistrar).should('contain', 'Registrar').click();
 
-    cy.get("form[class='styles__ContainerFormRegister-sc-7fhc7g-0 keVbpY'] input[placeholder='Informe seu e-mail']").type('jairotest@gmail.com', {force:true})
+    cy.get(elemento.email).type(user_1, {force:true})
 
-    cy.get("input[placeholder='Informe seu Nome']").type('jairo silva', {force: true})
+    cy.get(elemento.nome).type( name, {force: true})
 
-    cy.get("form[class='styles__ContainerFormRegister-sc-7fhc7g-0 keVbpY'] input[placeholder='Informe sua senha']").type('123456', {force: true})
+    cy.get(elemento.password).type( psw, {force: true})
 
-    cy.get("input[placeholder='Informe a confirmação da senha']").type('123456', {force: true})
+    cy.get(elemento.password).type( psw, {force: true})
 
     cy.get("#toggleAddBalance").click({force: true})
 
-    cy.get("button[class='style__ContainerButton-sc-1wsixal-0 CMabB button__child']").should('contain', 'Cadastrar').click({force: true})
+    cy.get(btnCadastrar).should('contain', 'Cadastrar').click({force: true})
 
     cy.get("#btnCloseModal").should('contain', 'Fechar').click({force: true})
 
-    cy.get("form[class='style__ContainerFormLogin-sc-1wbjw6k-0 eTrcYr'] input[placeholder='Informe seu e-mail']").type('jairotest@gmail.com', {force:true})
+    cy.get(elemento.email).type( user_1, {force:true})
 
-    cy.get("form[class='style__ContainerFormLogin-sc-1wbjw6k-0 eTrcYr'] input[placeholder='Informe sua senha']").type('123456', {force: true})
+    cy.get(elemento.password).type( psw, {force: true})
 
-    cy.get("button[class='style__ContainerButton-sc-1wsixal-0 otUnI button__child']").should('contain', 'Acessar').click({force: true})
+    cy.get(btnAcessar).should('contain', 'Acessar').click({force: true})
 
     
   });
-
-  // it('Realizar transferencia', () => {
-    
-    
-  // });
 });
 
